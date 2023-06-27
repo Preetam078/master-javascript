ebc237d7870b59ac5a6277c51ce34606a1b5a27f
preetam <preetammondal78@gmail.com>
2023-06-27 23:44:02 +0530

    done exercise

---
+++ Assignment1.js
@@ -0,0 +1,24 @@
+/*Q1. Add 2 Number */
+console.log(23 + 97)
+
+/*Q2. Add a sequence of 6 different numbers together.*/
+console.log(1+2+3+4+5+5)
+
+/*Q3. Print the solution to the following equation: (4 + 6 + 9) / 77*/
+console.log((4 + 6 + 9) / 77)
+
+/**Q4. Let’s use variables!*/
+let a = 10 
+console.log(a)
+console.log(9*a)
+let c
+let b = 7*c
+console.log(c) //Undefined because the value is not assigned
+console.log(b) //NaN comes only in undefined computation.
+
+
+/**Q5. Let’s use variables!*/
+const max = 57
+let actual = max - 13
+let percentage = actual/max
+console.log(percentage)
